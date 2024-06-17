9ab2f19cd52615e042ece2b5ceacadb8af22a845
Edwin Murari <edwin.murari@myxplor.com>
2024-06-17 13:17:46 +1000

    Added test to network layer

---
+++ network/src/test/java/com/edwin/network/AniListMediaNetworkTest.kt
@@ -0,0 +1,61 @@
+package com.edwin.network
+
+import com.apollographql.apollo3.ApolloClient
+import com.edwin.network.apollo.AniListMediaNetwork
+import com.edwin.network.type.MediaSeason
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class AniListMediaNetworkTest {
+
+    private lateinit var apolloClient: ApolloClient
+    private lateinit var networkDataSource: AniListMediaNetwork
+
+    private val mediaSeason = MediaSeason.FALL
+    private val seasonYear = 2024
+
+    @Before
+    fun setup() {
+        apolloClient = ApolloClient.Builder()
+            .serverUrl(BuildConfig.ANILIST_GRAPHQL_URL)
+            .build()
+        networkDataSource = AniListMediaNetwork(apolloClient, Dispatchers.IO)
+    }
+
+    @Test
+    fun `getTrendingAndPopularMedia - success with all fields having values`() = runTest {
+        val response = networkDataSource.getTrendingAndPopularMedia(mediaSeason, seasonYear)
+
+        response.data?.also { responseData ->
+            assertNull(response.errors)
+
+            with(responseData) {
+                trendingAnimeThisSeason?.media.shouldNotBeEmpty()
+                trendingMoviesThisSeason?.media.shouldNotBeEmpty()
+                popularAnimeThisSeason?.media.shouldNotBeEmpty()
+                topAnimeThisSeason?.media.shouldNotBeEmpty()
+                trendingAnimeAllTime?.media.shouldNotBeEmpty()
+                popularAnimeAllTime?.media.shouldNotBeEmpty()
+                topAnimeAllTime?.media.shouldNotBeEmpty()
+                popularMoviesThisSeason?.media.shouldNotBeEmpty()
+                topMoviesThisSeason?.media.shouldNotBeEmpty()
+                trendingMoviesAllTime?.media.shouldNotBeEmpty()
+                popularMoviesAllTime?.media.shouldNotBeEmpty()
+                topMoviesAllTime?.media.shouldNotBeEmpty()
+            }
+        } ?: fail("Response data is null")
+    }
+
+    private fun <T> List<T>?.shouldNotBeEmpty() {
+        assertNotNull("List should not be null", this)
+        assertTrue("List should not be empty", this!!.isNotEmpty())
+    }
+}
