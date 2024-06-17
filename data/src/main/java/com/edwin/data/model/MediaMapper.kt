0f9d35bd3ed79f5935f2db9531a5f4e2f9c112fd
Edwin Murari <edwin.murari@myxplor.com>
2024-06-17 13:58:19 +1000

    Updated Media fields to be nullable so it can be handled on the UI

--- data/src/main/java/com/edwin/data/model/MediaMapper.kt
+++ data/src/main/java/com/edwin/data/model/MediaMapper.kt
@@ -2,62 +2,66 @@ package com.edwin.data.model
 
 import com.edwin.network.GetTrendingAndPopularQuery
 import com.edwin.network.fragment.AnimeFragment
+import com.edwin.network.fragment.MediaFragment
 import com.edwin.network.fragment.MovieFragment
+import com.edwin.network.type.MediaSeason as NetworkMediaSeason
 
 fun GetTrendingAndPopularQuery.Data.asExternalModel() = MediaCollections(
-    trendingTvSeries = trendingAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    trendingMovies = trendingMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.toMovie() },
-    popularTvSeries = popularAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    topTvSeries = topAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    trendingTvSeriesAllTime = trendingAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    popularTvSeriesAllTime = popularAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    topTvSeriesAllTime = topAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.toTvSeries() },
-    popularMovies = popularMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.toMovie() },
-    topMovies = topMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.toMovie() },
-    trendingMoviesAllTime = trendingMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.toMovie() },
-    popularMoviesAllTime = popularMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.toMovie() },
-    topMoviesAllTime = topMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.toMovie() }
+    trendingTvSeries = trendingAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    trendingMovies = trendingMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.asExternalModel() },
+    popularTvSeries = popularAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    topTvSeries = topAnimeThisSeason?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    trendingTvSeriesAllTime = trendingAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    popularTvSeriesAllTime = popularAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    topTvSeriesAllTime = topAnimeAllTime?.media?.mapNotNull { it?.animeFragment?.asExternalModel() },
+    popularMovies = popularMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.asExternalModel() },
+    topMovies = topMoviesThisSeason?.media?.mapNotNull { it?.movieFragment?.asExternalModel() },
+    trendingMoviesAllTime = trendingMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.asExternalModel() },
+    popularMoviesAllTime = popularMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.asExternalModel() },
+    topMoviesAllTime = topMoviesAllTime?.media?.mapNotNull { it?.movieFragment?.asExternalModel() }
 )
 
-fun AnimeFragment.toTvSeries() = Media.TvSeries(
+fun AnimeFragment.asExternalModel() = Media.TvSeries(
     id = mediaFragment.id,
-    title = (mediaFragment.title?.english ?: mediaFragment.title?.romaji).orEmpty(),
-    description = mediaFragment.description.orEmpty(),
+    title = mediaFragment.getTitle(),
+    description = mediaFragment.description,
     coverImage = mediaFragment.coverImage?.large.takeUnless { it.isNullOrBlank() },
-    bannerImage = mediaFragment.thumbnail?.extraLarge.takeUnless { it.isNullOrBlank() },
+    bannerImage = mediaFragment.bannerImage.takeUnless { it.isNullOrBlank() },
     averageScore = mediaFragment.averageScore,
-    popularity = mediaFragment.popularity ?: 0,
-    startDate = mediaFragment.startDate?.year ?: 0,
+    popularity = mediaFragment.popularity,
+    startDate = mediaFragment.startDate?.year,
     averageColorHex = mediaFragment.coverImage?.color.takeUnless { it.isNullOrBlank() },
-    episodes = episodes ?: 0,
-    nextAiringEpisode = nextAiringEpisode?.toAnimeNextAiringEpisode()
+    episodesCount = episodes,
+    nextAiringEpisode = nextAiringEpisode?.asExternalModel()
 )
 
-private fun AnimeFragment.NextAiringEpisode?.toAnimeNextAiringEpisode() = this?.let {
+private fun MediaFragment.getTitle() = title?.english ?: title?.romaji ?: title?.native
+
+private fun AnimeFragment.NextAiringEpisode?.asExternalModel() = this?.let {
     Media.TvSeries.NextAiringEpisode(
         episode = it.episode,
         timeUntilAiring = it.timeUntilAiring
     )
 }
 
-fun MovieFragment.toMovie() = Media.Movie(
+fun MovieFragment.asExternalModel() = Media.Movie(
     id = mediaFragment.id,
-    title = (mediaFragment.title?.english ?: mediaFragment.title?.romaji).orEmpty(),
-    description = mediaFragment.description.orEmpty(),
-    coverImage = mediaFragment.coverImage?.large.orEmpty(),
-    bannerImage = mediaFragment.thumbnail?.extraLarge.orEmpty(),
+    title = mediaFragment.getTitle(),
+    description = mediaFragment.description,
+    coverImage = mediaFragment.coverImage?.large.takeUnless { it.isNullOrBlank() },
+    bannerImage = mediaFragment.bannerImage.takeUnless { it.isNullOrBlank() },
     averageScore = mediaFragment.averageScore,
-    popularity = mediaFragment.popularity ?: 0,
-    startDate = mediaFragment.startDate?.year ?: 0,
-    averageColorHex = mediaFragment.coverImage?.color,
-    duration = duration ?: 0
+    popularity = mediaFragment.popularity,
+    startDate = mediaFragment.startDate?.year,
+    averageColorHex = mediaFragment.coverImage?.color.takeUnless { it.isNullOrBlank() },
+    duration = duration
 )
 
-fun mapToNetworkMediaSeason(season: MediaSeason): com.edwin.network.type.MediaSeason {
-    return when (season) {
-        MediaSeason.WINTER -> com.edwin.network.type.MediaSeason.WINTER
-        MediaSeason.SPRING -> com.edwin.network.type.MediaSeason.SPRING
-        MediaSeason.SUMMER -> com.edwin.network.type.MediaSeason.SUMMER
-        MediaSeason.FALL -> com.edwin.network.type.MediaSeason.FALL
+fun MediaSeason.asNetworkModel(): NetworkMediaSeason {
+    return when (this) {
+        MediaSeason.WINTER -> NetworkMediaSeason.WINTER
+        MediaSeason.SPRING -> NetworkMediaSeason.SPRING
+        MediaSeason.SUMMER -> NetworkMediaSeason.SUMMER
+        MediaSeason.FALL -> NetworkMediaSeason.FALL
     }
 }
