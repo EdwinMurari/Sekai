914f7c17b242ad4068553fc2decc3f30131b5d96
Edwin Murari <edwin.murari@myxplor.com>
2024-06-17 17:39:58 +1000

    Replaced navDrawer with tabbed navigation

--- tv/src/main/java/com/edwin/sekai/ui/feature/extensions/ExtensionsScreen.kt
+++ tv/src/main/java/com/edwin/sekai/ui/feature/extensions/ExtensionsScreen.kt
@@ -1,11 +1,12 @@
 package com.edwin.sekai.ui.feature.extensions
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Text
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
-fun ExtensionsScreen() {
+fun ExtensionsScreen(modifier: Modifier = Modifier) {
     Text("Extension screen TODO")
 }
---
+++ tv/src/main/java/com/edwin/sekai/ui/feature/home/HomeViewModel.kt
@@ -0,0 +1,23 @@
+package com.edwin.sekai.ui.feature.home
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi
+import androidx.lifecycle.viewmodel.compose.saveable
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    @OptIn(SavedStateHandleSaveableApi::class)
+    var selectedTab by savedStateHandle.saveable { mutableStateOf(TabNavOption.Home) }
+        private set
+
+    fun setTab(tabNavOption: TabNavOption) {
+        selectedTab = tabNavOption
+    }
+}
