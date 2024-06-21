01e578da6b5a8656451d19ab7e785673ee0bc28e
Edwin Murari <edwin.murari@myxplor.com>
2024-06-21 15:38:29 +1000

    Inject jikan service

--- network/jikan/src/main/java/com/edwin/network/jikan/di/NetworkModule.kt
+++ network/jikan/src/main/java/com/edwin/network/jikan/di/NetworkModule.kt
@@ -1,14 +1,18 @@
 package com.edwin.network.jikan.di
 
 import com.edwin.network.jikan.BuildConfig
+import com.edwin.network.jikan.service.JikanService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.Call
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -33,4 +37,22 @@ object NetworkModule {
                 },
         )
         .build()
+
+    @Provides
+    @Singleton
+    fun provideService(
+        json: Json,
+        okhttpCallFactory: dagger.Lazy<Call.Factory>
+    ): JikanService = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_API_URL)
+        // We use callFactory lambda here with dagger.Lazy<Call.Factory>
+        // to prevent initializing OkHttp on the main thread.
+        .callFactory { okhttpCallFactory.get().newCall(it) }
+        .addConverterFactory(
+            json.asConverterFactory(
+                "application/json; charset=UTF8".toMediaType()
+            )
+        )
+        .build()
+        .create(JikanService::class.java)
 }
--- network/jikan/src/main/java/com/edwin/network/jikan/retrofit/RetrofitJikanNetwork.kt
+++ network/jikan/src/main/java/com/edwin/network/jikan/retrofit/RetrofitJikanNetwork.kt
@@ -1,43 +1,15 @@
 package com.edwin.network.jikan.retrofit
 
-import com.edwin.network.jikan.BuildConfig
 import com.edwin.network.jikan.JikanNetworkDataSource
 import com.edwin.network.jikan.model.JikanEpisodesResponse
-import kotlinx.serialization.json.Json
-import okhttp3.Call
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
+import com.edwin.network.jikan.service.JikanService
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private interface JikanService {
-
-    @GET("anime/{malId}/episodes")
-    suspend fun getAnimeEpisodes(@Path("malId") malId: Int): JikanEpisodesResponse
-}
-
 @Singleton
 internal class RetrofitJikanNetwork @Inject constructor(
-    json: Json,
-    okhttpCallFactory: dagger.Lazy<Call.Factory>,
+    private val service: JikanService
 ) : JikanNetworkDataSource {
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BuildConfig.BASE_API_URL)
-        // We use callFactory lambda here with dagger.Lazy<Call.Factory>
-        // to prevent initializing OkHttp on the main thread.
-        .callFactory { okhttpCallFactory.get().newCall(it) }
-        .addConverterFactory(
-            json.asConverterFactory(
-                "application/json; charset=UTF8".toMediaType()
-            )
-        )
-        .build()
-
-    private val service: JikanService = retrofit.create(JikanService::class.java)
 
     override suspend fun getEpisodeForAnime(malId: Int): JikanEpisodesResponse {
         return service.getAnimeEpisodes(malId)
