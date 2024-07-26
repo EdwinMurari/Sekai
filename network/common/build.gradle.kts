7f3558ca7bb5af58663a3eaa32be732d03dd2c3e
Edwin Murari <edwin.murari@myxplor.com>
2024-07-26 12:08:27 +1000

    Removed the unnecessary dependency on the ':network:extensions' project from common module

--- network/common/build.gradle.kts
+++ network/common/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":network:extensions"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
